7338b0d33d89ce7442ef1e1c82da2d6ac2d7b30c
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-02-23 00:41:38 -0600

    Finished Navigation Menu

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';  
 
-import HomePage from '@/views/HomePage.vue'
+import HomePage from '@/views/HomePage.vue';  
+import CardDetail from '@/views/CardDetail.vue';  
+import LoginPage from '@/views/LoginPage.vue';  
+import SettingsPage from '@/views/SettingsPage.vue';  
 
-const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-]
+const routes = [  
+  { path: '/', name: 'Home', component: HomePage },  
+  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },  
+  { path: '/employees/:id', name: 'CardDetail', component: CardDetail },  
+  { path: '/login', name: 'LoginPage', component: LoginPage },  
+  { path: '/settings', name: 'SettingsPage', component: SettingsPage },  
+];  
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+const router = createRouter({  
+  history: createWebHistory(),  
+  routes,  
+});  
 
-export default router
+export default router;
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+
+<template>
+    Login Page
+</template>
